3f44e8ad05d4a22b166a5f1289fb958d5cd1fa70
John Kenneth <johnkennethagpaoa@gmail.com>
2023-04-17 13:27:29 +0800

    added sounds to buttons

--- game.js
+++ game.js
@@ -2,13 +2,19 @@ var gamePattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
 var userClickedPattern = [];
 
+
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
 
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    playSound(randomChosenColour);
+}
+
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
 }
 
@@ -16,5 +22,5 @@ function nextSequence() {
 $(".btn").click(function () {
     var userChosenColour = this.id;
     userClickedPattern.push(userChosenColour);
-    console.log(userClickedPattern);
+    playSound(userChosenColour);
 });
