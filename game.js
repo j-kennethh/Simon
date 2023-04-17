c049ecc38a43ddb02cb457cea0c471e14b2a8665
John Kenneth <johnkennethagpaoa@gmail.com>
2023-04-17 13:17:25 +0800

    added animations and sounds

--- game.js
+++ game.js
@@ -1,9 +1,13 @@
 var gamePattern = []; 
 var buttonColours = ["red", "blue", "green", "yellow"];
 
-var randomChosenColour = buttonColours[nextSequence()];
-gamePattern.push(randomChosenColour);
 
 function nextSequence() {
-    return Math.floor(Math.random() * 4);
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    audio.play();
 }
