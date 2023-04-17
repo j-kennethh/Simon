35187ecd3f984a93b6ddb89f63f8087f0e1b1bca
John Kenneth <johnkennethagpaoa@gmail.com>
2023-04-17 13:47:24 +0800

    implemented game start

--- game.js
+++ game.js
@@ -1,6 +1,17 @@
 var gamePattern = []; 
 var buttonColours = ["red", "blue", "green", "yellow"];
 var userClickedPattern = [];
+var gameStarted = false;
+var level = 0;
+
+
+$("body").keydown(function () {
+    if (!gameStarted) {
+        gameStarted = true;
+        nextSequence();
+        $("#level-title").text("Level " + level);
+    }
+});
 
 
 $(".btn").click(function () {
@@ -16,6 +27,8 @@ function nextSequence() {
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
 
+    level++;
+    $("#level-title").text("Level " + level);
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
     playSound(randomChosenColour);
 }
